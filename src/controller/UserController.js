a158f696e5cbe8bd768b72062f7eddecb62763c4
Rabiul Islam Rocky <152948126+devsrocky@users.noreply.github.com>
2026-01-08 15:19:35 +0600

    remove update cookie

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -10,9 +10,7 @@ exports.UserLogin = async (req, res) => {
     if(data?.['status'] === 'login'){
         let cookieOption = {
             httpOnly: true,
-            expires: new Date(Date.now() + 72 * 60 * 60 * 1000),
-            sameSite: 'none',
-            secure: true,
+            expires: new Date(Date.now() + 72 * 60 * 60 * 1000)
         }
         res.cookie('token', data['token'], cookieOption)
         return res.status(200).json(data)
