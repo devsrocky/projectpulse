eb63afcbfc95bacef185d3bf5c349a9f31f024f9
Rabiul Islam Rocky <152948126+devsrocky@users.noreply.github.com>
2026-01-09 20:55:26 +0600

    set trust proxy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = new express();
 const router = require('./src/route/api');
 
 // IMPORT ALL PACKAGE
+app.set('trust proxy', 1);
 const path = require('path');
 const helmet =require('helmet');
 const cors = require('cors');
