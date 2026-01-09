c8f06afcbe6723eca5100d8c20bc4a1dd0d9465a
Rabiul Islam Rocky <152948126+devsrocky@users.noreply.github.com>
2026-01-09 20:52:35 +0600

    Fix cookie expiration date formatting

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -11,7 +11,7 @@ exports.UserLogin = async (req, res) => {
         let cookieOption = {
             httpOnly: true,
             secure: true,
-            expires: new Date(Date.now() + 72 * 60 * 60 * 1000)
+            expires: new Date(Date.now() + 72 * 60 * 60 * 1000),
             sameSite: 'none',
         }
 
