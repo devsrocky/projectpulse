15b594f2aff37af721ae7385c8671c482395537f
Rabiul Islam Rocky <152948126+devsrocky@users.noreply.github.com>
2026-01-09 15:43:52 +0600

    Update cookie options

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -8,10 +8,18 @@ exports.CreateUser = async (req, res) => {
 exports.UserLogin = async (req, res) => {
     let data = await UserLoginService(req);
     if(data?.['status'] === 'login'){
-        let cookieOption = {
-            httpOnly: true,
-            expires: new Date(Date.now() + 72 * 60 * 60 * 1000)
-        }
+        // let cookieOption = {
+        //     httpOnly: true,
+        //     expires: new Date(Date.now() + 72 * 60 * 60 * 1000)
+        // }
+
+        let cookieOptions = {
+              httpOnly: true,                                // secure & middleware readable
+              secure: process.env.NODE_ENV === 'production', // HTTPS required in production
+              sameSite: 'none',                              // important for cross-origin
+              maxAge: 72 * 60 * 60 * 1000,                  // 72 hours
+              path: '/',                                     // available on all routes
+        };
         res.cookie('token', data['token'], cookieOption)
         return res.status(200).json(data)
     }else{
